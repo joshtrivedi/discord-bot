73a3f6251b53005779c00d8f7f86d6cbf50ecc97
Josh Trivedi <josh.trivedi@gmail.com>
2021-03-05 23:48:06 +0530

    seperate code for purging

--- commands/purge.js
+++ commands/purge.js
@@ -1,14 +1,22 @@
-/*module.exports = {
-    if (msg.content.substring(0, 1) == '!') {
-        if ((msg.member.roles.cache.has(moderator_role)) && (msg.content.substring(1).split(" ")[0] == "purge" )) {
-            var num = msg.content.substring(1).split(" ")[1];
-            if (isNaN(num)) msg.reply("the argument that you entered after the command isn't a number");
-            else {
-                    msg.channel.bulkDelete(num, true).catch(err =>{
-                    console.error(err);
-                    msg.channel.send('there was a problem pruning messages from this channel');
-                })
+const moderator_role = require('../config.json')
+
+module.exports = {
+    name: "purge",
+    description: "Delete certain number of commands",
+    async execute(msg, args) {
+        if (msg.content.substring(0, 1) == '!') {
+            if ((msg.member.roles.cache.find(m => m.name.toLowerCase() === moderator_role))) {
+                var num = args
+                if (isNaN(num)) msg.reply("the argument that you entered after the command isn't a number");
+                else {
+                    msg.channel.bulkDelete(num, true).catch(err => {
+                        console.error(err);
+                        msg.channel.send('there was a problem pruning messages from this channel');
+                    })
+                }
+            } else {
+                msg.channel.send('only moderators can use that')
             }
         }
     }
-})*/
+}

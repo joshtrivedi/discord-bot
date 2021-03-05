e6b3510b8e8fa716750eb657f2048d601d54f240
Josh Trivedi <josh.trivedi@gmail.com>
2021-03-05 23:48:32 +0530

    index file update with command handler

--- discord-bot.js
+++ discord-bot.js
@@ -1,20 +1,53 @@
 const Discord = require('discord.js');
 const {
     moderator_role,
-    testing_channel
+    testing_channel,
+    prefix
 } = require('./config.json')
+const dotenv = require('dotenv')
+dotenv.config()
 const {
     EventEmitter
 } = require('events');
 const {
     userInfo
 } = require('os');
+const fs = require('fs')
 const bot = new Discord.Client();
+
+
+bot.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'))
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`)
+    bot.commands.set(command.name, command);
+}
 
 bot.on('ready', () => {
     console.log('This bot is working');
 })
 
+bot.on('message', async (msg) => {
+    if (!msg.content.startsWith(prefix) || msg.author.bot) return
+
+    const args = msg.content.slice(prefix.length).split(/ +/)
+    const command = args.shift().toLowerCase()
+
+    if (command === 'ping') {
+        bot.commands.get('ping').execute(msg)
+    } else if (command === 'help') {
+        let noOfCommands = "Available commands: \n"
+        const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'))
+        for (const file of commandFiles) {
+            const command = require(`./commands/${file}`)
+            noOfCommands += (prefix + command.name, " : ", command.description+"\n")
+        }
+        msg.channel.send(noOfCommands) 
+    } else if (command === 'purge') {
+        bot.commands.get('purge').execute(msg, args)
+    }
+})
 
 
 bot.login(process.env.CONFIG_TOKEN);

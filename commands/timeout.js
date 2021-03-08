eadb9735879913fc9b04739eff6f2583706dac88
Josh Trivedi <josh.trivedi@gmail.com>
2021-03-08 21:51:51 +0530

    timeout wireframe

---
+++ commands/timeout.js
@@ -0,0 +1,10 @@
+
+
+module.exports = {
+    name: "timeout",
+    description: "times out a person for an illicit activity for the given amount of time",
+    access: "moderators",
+    async execute (msg, args){
+        
+    }
+}

7b8d3022f4535509628356255fc9b9358c8c55c9
Josh Trivedi <josh.trivedi@gmail.com>
2021-03-08 09:42:13 +0530

    purge fix

--- commands/purge.js
+++ commands/purge.js
@@ -1,23 +1,29 @@
-const moderator_role = require('../config.json')
+const {
+    MessageAttachment
+} = require('discord.js');
+const {
+    moderator_role
+} = require('../config.json')
 
 module.exports = {
     name: "purge",
     description: "Delete certain number of commands",
-    access: "mods",
     async execute(msg, args) {
-        if (msg.content.substring(0, 1) == '!') {
-            if ((msg.member.roles.cache.find(m => m.name.toLowerCase() === moderator_role))) {
-                var num = args
-                if (isNaN(num)) msg.reply("the argument that you entered after the command isn't a number");
-                else {
-                    msg.channel.bulkDelete(num, true).catch(err => {
-                        console.error(err);
-                        msg.channel.send('there was a problem pruning messages from this channel');
-                    })
-                }
-            } else {
-                msg.channel.send('only moderators can use that')
+        if(!args.length === 1){
+            msg.channel.send('Please enter only one argument!')
+            return
+        }
+        if(msg.member.roles.cache.find(((m) => moderator_role.find(f=>f.includes(m.name.toLowerCase()))))) {
+            var num = args[0]
+            if (isNaN(num)) msg.reply("the argument that you entered after the command isn't a number");
+            else {
+                msg.channel.bulkDelete(num, true).catch(err => {
+                    console.error(err);
+                    msg.channel.send('there was a problem pruning messages from this channel');
+                })
             }
+        }else{
+            msg.channel.send('You need to have administrator privileges for that!')
         }
     }
 }

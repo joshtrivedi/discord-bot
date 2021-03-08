6a8911c76e04d914390a4727093235f17e3a84de
Josh Trivedi <josh.trivedi@gmail.com>
2021-03-08 21:53:35 +0530

    command handler fix

--- discord-bot.js
+++ discord-bot.js
@@ -36,9 +36,37 @@ bot.on('message', async (msg) => {
 
     if (command === 'ping') {
         bot.commands.get('ping').execute(msg)
-    } else if (command === 'help') {
-        let noOfCommands = "Available commands: \n"
+    } else if (command === 'help' || command === 'commands') {
         const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'))
+        if(args[0] === 'filter') {
+            switch(args[1]){
+                case 'name':
+                    msg.channel.send('Do !commands and you will find the name of all commands')
+                    break;
+                case 'access':
+                    let filtered_everyone = '**Commands accessible to everyone:** \n'
+                    let filtered_mods = '**Commands accessible to mods:** \n'
+                    //const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'))
+                    for (const file of commandFiles) {
+                        const command = require(`./commands/${file}`)
+                        if(command.access === 'everyone'){
+                            filtered_everyone += ("`"+command.name+" `: "+command.description+"\n")
+                        } else if (command.access === 'moderators'){
+                            filtered_mods += ("`"+command.name+" `: "+command.description+"\n")
+                        }
+                        //noOfCommands += (command.name+" : "+command.description+" : ` access : "+command.access+"`\n")
+                    }
+                    msg.channel.send(filtered_everyone)
+                    msg.channel.send(filtered_mods)
+                    break;
+                default:
+                    msg.channel.send('wrong arguments, please enter either `!commands filter name` or `!commands filter access`')
+                    break;
+            }
+            return
+        }
+        let noOfCommands = "Available commands: \n"
+        //const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'))
         for (const file of commandFiles) {
             const command = require(`./commands/${file}`)
             noOfCommands += (command.name+" : "+command.description+" : ` access : "+command.access+"`\n")
@@ -46,8 +74,19 @@ bot.on('message', async (msg) => {
         msg.channel.send(noOfCommands) 
     } else if (command === 'purge') {
         bot.commands.get('purge').execute(msg, args)
+    } else if (command === 'mute') {
+        bot.commands.get('mute').execute(msg, args)
+    } else if (command === 'ban') {
+        bot.commands.get('ban').execute(msg, args)
+    } else if (command === 'timeout') {
+        bot.commands.get('timeout').execute(msg,args)
+    } else if (command === 'compliment') {
+        bot.commands.get('compliment').execute(msg, args)
+    } else if (command === 'mv' || command === 'motivation') {
+        bot.commands.get('motivation').execute(msg, args)
     }
 })
+ 
 
 
 bot.login(process.env.CONFIG_TOKEN);

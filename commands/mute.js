1ca5e862404fdbbacd94436e6de2bf085e0f61f6
Josh Trivedi <josh.trivedi@gmail.com>
2021-03-13 23:50:00 +0530

    updated dependencies

--- commands/mute.js
+++ commands/mute.js
@@ -3,6 +3,9 @@ module.exports = {
     description: "mute a user for a given amout of time",
     access: "moderators",
     async execute (msg, args){
-        
+     const {
+         content, channel, author
+     } = msg
+     
     }
 }

803ce56748e39c1110e7a7f12a0fef4d0d739639
Josh Trivedi <josh.trivedi@gmail.com>
2021-03-17 23:43:16 +0530

    ban command wireframe

--- commands/ban.js
+++ commands/ban.js
@@ -1,10 +1,54 @@
+const {
+    moderator_role
+} = require('../config.json')
 
+const Discord = require('discord.js')
+
+const reg = /(<@!)\d{18}(>)/
 
 module.exports = {
     name: "ban",
     description: "ban a user with a reason specified",
     access: "moderators",
-    async execute (msg, args){
+    async execute (client,message,args){
+        /*const {
+            content, author, channel
+        } = msg
+        let mod = msg.member.roles.cache.find(r=>moderator_role.includes(r.name.toLowerCase()))
+        if(!mod) return*/
+        if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send('You can\'t use that!')
+        if(!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send('I don\'t have the right permissions.')
+
+        const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+
+        if(!args[0]) return message.channel.send('Please specify a user');
+
+        if(!member) return message.channel.send('Can\'t seem to find this user. Sorry \'bout that :/');
+        if(!member.bannable) return message.channel.send('This user can\'t be banned. It is either because they are a mod/admin, or their highest role is higher than mine');
+
+        if(member.id === message.author.id) return message.channel.send('Bruh, you can\'t ban yourself!');
+
+        let reason = args.slice(1).join(" ");
+
+        if(!reason) reason = 'Unspecified';
+
+        member.ban(`${reason}`).catch(err => { 
+          message.channel.send('Something went wrong')
+            console.log(err)
+        })
+
+        const banembed = new Discord.MessageEmbed()
+        .setTitle('Member Banned')
+        .setThumbnail(member.user.displayAvatarURL())
+        .addField('User Banned', member)
+        .addField('Kicked by', message.author)
+        .addField('Reason', reason)
+        .setFooter('Time kicked', client.user.displayAvatarURL())
+        .setTimestamp()
+
+        message.channel.send(banembed);
+
+
         
     }
 }

2d7ae2912ded941c6c0ca080904f62eb9357cc65
Josh Trivedi <josh.trivedi@gmail.com>
2021-03-18 01:21:41 +0530

    ban update - time add

--- commands/ban.js
+++ commands/ban.js
@@ -20,8 +20,10 @@ module.exports = {
         if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send('You can\'t use that!')
         if(!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send('I don\'t have the right permissions.')
 
-        const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+        const member = message.mentions.members.first();
         const duration = args[1]
+        let reg = /d+/
+        if(!reg.test(duration)) duration = 0
         if(!args[0]) return message.channel.send('Please specify a user');
 
         if(!member) return message.channel.send('Can\'t seem to find this user. Sorry \'bout that :/');
@@ -29,7 +31,7 @@ module.exports = {
 
         if(member.id === message.author.id) return message.channel.send('Bruh, you can\'t ban yourself!');
         
-
+        
         let reason = args.slice(2).join(" ");
 
         if(!reason) reason = 'Unspecified';

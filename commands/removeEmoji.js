5f35b396abf8033308d72f319263bb3a0c2eb91d
Josh Trivedi <josh.trivedi@gmail.com>
2022-02-23 17:10:05 +0530

    TODO add command to remove emoji from server guild

---
+++ commands/removeEmoji.js
@@ -0,0 +1,10 @@
+const discord = require('discord.js');
+
+module.exports = {
+    name: "removeEmoji",
+    description: "Removed emoji from server guild",
+    access: "moderators",
+    async execute(msg, args){
+        
+    }
+}
